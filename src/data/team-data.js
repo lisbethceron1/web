7d74a5e83df2b9ad05631a7abc97c8bce20099e7
lisbethceron1 <flceron0@misena.edu.co>
2024-03-05 15:48:52 -0500

    cards

--- src/data/team-data.js
+++ src/data/team-data.js
@@ -1,6 +1,6 @@
 export const teamData = [
   {
-    img: "/img/LogoPNG.png",
+    img: "/img/bombilla.png",
     name: "Nosotros",
     position: "Descubre una nueva forma de aprender con  clases personalizadas y flexibilidad total, te ofrecemos la oportunidad de crecer académicamente a tu ritmo y conveniencia. En Educatio, nos esforzamos por proporcionarte una experiencia educativa sin igual.",
     socials: [
@@ -8,7 +8,7 @@ export const teamData = [
     ],
   },
   {
-    img: "/img/team-2.jpg",
+    img: "/img/libro.png",
     name: "Clases Personalizadas",
     position: "Nuestras clases personalizadas permiten a los estudiantes aprender a su propio ritmo, recibir retroalimentación específica y utilizar recursos adaptados a sus necesidades, lo que aumenta su confianza y maximiza su potencial académico.",
     socials: [
@@ -16,7 +16,7 @@ export const teamData = [
     ],
   },
   {
-    img: "/img/team-3.jpg",
+    img: "/img/monitor-de-computadora.png",
     name: "Modalidad Virtual o presencial",
     position: "Ofrecemos opciones flexibles de clases virtuales y presenciales para adaptarnos al estilo de vida de cada estudiante. Las clases virtuales brindan conveniencia, mientras que las presenciales ofrecen una experiencia más interactiva.",
     socials: [
@@ -24,7 +24,7 @@ export const teamData = [
     ],
   },
   {
-    img: "/img/team-1.jpg",
+    img: "/img/ahorrar-dinero.png",
     name: "Contraoferta",
     position: "En nuestra plataforma, los estudiantes tienen la libertad de publicar sus requisitos de clase y recibir múltiples ofertas de instructores. Esta modalidad única permite a los estudiantes comparar precios, ubicaciones y condiciones de enseñanza antes de comprometerse.",
     socials: [
