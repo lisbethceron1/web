a57ba74db1ee326cb71ea58c6318b449c210c317
lisbethceron1 <flceron0@misena.edu.co>
2024-03-05 15:28:07 -0500

    informacion

--- src/data/team-data.js
+++ src/data/team-data.js
@@ -1,137 +1,39 @@
 export const teamData = [
   {
-    img: "/img/team-1.jpg",
-    name: "Manuel Solarte",
-    position: "Web Developer",
+    img: "/img/LogoPNG.png",
+    name: "Nosotros",
+    position: "Descubre una nueva forma de aprender con  clases personalizadas y flexibilidad total, te ofrecemos la oportunidad de crecer académicamente a tu ritmo y conveniencia. En Educatio, nos esforzamos por proporcionarte una experiencia educativa sin igual.",
     socials: [
-      {
-        color: "light-blue",
-        name: "twitter",
-      },
-      {
-        color: "blue",
-        name: "linkedin",
-      },
-      {
-        color: "pink",
-        name: "dribbble",
-      },
+      
     ],
   },
   {
     img: "/img/team-2.jpg",
-    name: "Jhon Rivera",
-    position: "Marketing Specialist",
+    name: "Clases Personalizadas",
+    position: "Nuestras clases personalizadas permiten a los estudiantes aprender a su propio ritmo, recibir retroalimentación específica y utilizar recursos adaptados a sus necesidades, lo que aumenta su confianza y maximiza su potencial académico.",
     socials: [
-      {
-        color: "light-blue",
-        name: "twitter",
-      },
-      {
-        color: "blue",
-        name: "linkedin",
-      },
-      {
-        color: "pink",
-        name: "dribbble",
-      },
+     
     ],
   },
   {
     img: "/img/team-3.jpg",
-    name: "Sebastian Rivera",
-    position: "UI/UX Designer",
+    name: "Modalidad Virtual o presencial",
+    position: "Ofrecemos opciones flexibles de clases virtuales y presenciales para adaptarnos al estilo de vida de cada estudiante. Las clases virtuales brindan conveniencia, mientras que las presenciales ofrecen una experiencia más interactiva.",
     socials: [
-      {
-        color: "light-blue",
-        name: "twitter",
-      },
-      {
-        color: "blue",
-        name: "linkedin",
-      },
-      {
-        color: "pink",
-        name: "dribbble",
-      },
+      
     ],
   },
   {
     img: "/img/team-1.jpg",
-    name: "Lisbeth Ceron",
-    position: "Web Developer",
+    name: "Contraoferta",
+    position: "En nuestra plataforma, los estudiantes tienen la libertad de publicar sus requisitos de clase y recibir múltiples ofertas de instructores. Esta modalidad única permite a los estudiantes comparar precios, ubicaciones y condiciones de enseñanza antes de comprometerse.",
     socials: [
-      {
-        color: "light-blue",
-        name: "twitter",
-      },
-      {
-        color: "blue",
-        name: "linkedin",
-      },
-      {
-        color: "pink",
-        name: "dribbble",
-      },
+      
     ],
   },
-  {
-    img: "/img/team-2.jpg",
-    name: "Valeria Fernandez",
-    position: "Marketing Specialist",
-    socials: [
-      {
-        color: "light-blue",
-        name: "twitter",
-      },
-      {
-        color: "blue",
-        name: "linkedin",
-      },
-      {
-        color: "pink",
-        name: "dribbble",
-      },
-    ],
-  },
-      {
-      img: "/img/team-1.jpg",
-      name: "Brayan Viveros",
-      position: "Web Developer",
-      socials: [
-        {
-          color: "light-blue",
-          name: "twitter",
-        },
-        {
-          color: "blue",
-          name: "linkedin",
-        },
-        {
-          color: "pink",
-          name: "dribbble",
-        },
-      ],
-    },
-    {
-      img: "/img/team-2.jpg",
-      name: "Johan Inzandara",
-      position: "Marketing Specialist",
-      socials: [
-        {
-          color: "light-blue",
-          name: "twitter",
-        },
-        {
-          color: "blue",
-          name: "linkedin",
-        },
-        {
-          color: "pink",
-          name: "dribbble",
-        },
-      ],
-  },
+
+   
+ 
 ];
 
 export default teamData;
